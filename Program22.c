a86fefa1601ae4584ee69b60769b8d495e8cabb1
mohitpagar <125683789+mohitpagar@users.noreply.github.com>
2023-03-01 01:09:35 +0530

    Add files via upload

---
+++ Program22.c
@@ -0,0 +1,20 @@
+// Demonstartion of iteration using for loop
+
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= 5; iCnt++)
+    {
+        printf("Marvellous : %d\n",iCnt);
+    }    
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
